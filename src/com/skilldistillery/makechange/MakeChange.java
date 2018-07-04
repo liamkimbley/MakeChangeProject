e7df927bb0e7be0fe6702b2b0fa4234689b8d890
liamkimbley <liam.kimbley@gmail.com>
2018-07-04 16:21:29 -0600

    reformatted code

--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -19,43 +19,39 @@ public class MakeChange {
 			String given = sc.next();
 			if (given.equalsIgnoreCase("QUIT")) {
 				break;
-			}			
+			}
 			double price = convertToDouble(cost);
 			double money = convertToDouble(given);
 //			System.out.println(money + " \t" + price);
-			
+
 			if (money == price) {
 				System.out.println("Exact amount. No change needed.");
 				System.out.println();
-			}
-			else if (money < price) {
+			} else if (money < price) {
 				double need = (price - money);
 				System.out.printf("Too little given. Need: $%.2f\n", need);
 				System.out.println();
-			}
-
-			else {
+			} else {
 				calculateChange(money, price);
 			}
 		}
-		
+
 		System.out.println("Goodbye.");
 		sc.close();
 	}
-	
+
 	public static void calculateChange(double money, double price) {
-		
+
 		double change = (money - price);
 		double twenty, one, quarter, dime, penny;
 		System.out.printf("$%.2f owed.\n", change);
-		twenty =(change/20);
+		twenty = (change / 20);
 		if (change >= 20.0) {
-			twenty = (int)twenty;
+			twenty = (int) twenty;
 			if (twenty == 1) {
 				System.out.print("1 twenty dollar bill, ");
-			}
-			else {
-				System.out.print((int)twenty + " twenty dollar bills, ");
+			} else {
+				System.out.print((int) twenty + " twenty dollar bills, ");
 			}
 			change = change % 20;
 		}
@@ -69,32 +65,29 @@ public class MakeChange {
 			change = change % 5;
 		}
 		if (change >= 1) {
-			one = (int)change;
+			one = (int) change;
 			if (one == 1) {
 				System.out.print("1 one dollar bill, ");
-			}
-			else {
-				System.out.print((int)one + " one dollar bills, ");
+			} else {
+				System.out.print((int) one + " one dollar bills, ");
 			}
 			change = change % 1;
 		}
 		if (change >= .25) {
-			quarter = (int)(change/.25);
+			quarter = (int) (change / .25);
 			if (quarter == 1) {
 				System.out.print("1 quarter, ");
-			}
-			else {
-				System.out.print((int)quarter + " quarters, ");
+			} else {
+				System.out.print((int) quarter + " quarters, ");
 			}
 			change = change % .25;
 		}
 		if (change >= .10) {
-			dime = (int)(change/.10);
+			dime = (int) (change / .10);
 			if (dime == 1) {
 				System.out.print("1 dime, ");
-			}
-			else {
-				System.out.print((int)dime + " dimes, ");
+			} else {
+				System.out.print((int) dime + " dimes, ");
 			}
 			change = change % .10;
 		}
@@ -103,21 +96,20 @@ public class MakeChange {
 			change = change % .045;
 		}
 		if (change >= .006) {
-			penny = (int)(change/.009);
+			penny = (int) (change / .009);
 //			System.out.print(penny);
 			if (penny == 1) {
 				System.out.println("1 penny.");
-			}
-			else {
-				System.out.println(((int)penny) + " pennies.");
+			} else {
+				System.out.println(((int) penny) + " pennies.");
 			}
 		}
 //		System.out.println(change);
 	}
-	
+
 	public static double convertToDouble(String word) {
 		double num = Double.parseDouble(word);
 		return num;
 	}
-	
+
 }
