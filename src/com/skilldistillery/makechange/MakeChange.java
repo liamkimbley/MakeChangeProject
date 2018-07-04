b572a477389d2514c358b5ef61d7fb88dcf7261d
liamkimbley <liam.kimbley@gmail.com>
2018-07-04 16:18:43 -0600

    change code complete

--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -11,27 +11,32 @@ public class MakeChange {
 		while (true) {
 			System.out.println("To Quit, please type \"QUIT\".");
 			System.out.print("Please enter the price of the item: ");
-			String price = sc.next();
-			if (price.equalsIgnoreCase("QUIT")) {
+			String cost = sc.next();
+			if (cost.equalsIgnoreCase("QUIT")) {
 				break;
 			}
 			System.out.print("Please enter the amount given: ");
-			String money = sc.next();
-			if (money.equalsIgnoreCase("QUIT")) {
+			String given = sc.next();
+			if (given.equalsIgnoreCase("QUIT")) {
 				break;
 			}			
-			if (money.equals(price)) {
-				System.out.println("Exact amount. No change needed.");
-			}
+			double price = convertToDouble(cost);
+			double money = convertToDouble(given);
+//			System.out.println(money + " \t" + price);
 			
-//			else if (money < price) {
-//				double need = (price - money);
-//				System.out.printf("Too little given. Need: $%.2f\n", need);
-//			}
-//
-//			else {
-//				calculateChange(money, price);
-//			}
+			if (money == price) {
+				System.out.println("Exact amount. No change needed.");
+				System.out.println();
+			}
+			else if (money < price) {
+				double need = (price - money);
+				System.out.printf("Too little given. Need: $%.2f\n", need);
+				System.out.println();
+			}
+
+			else {
+				calculateChange(money, price);
+			}
 		}
 		
 		System.out.println("Goodbye.");
@@ -108,37 +113,11 @@ public class MakeChange {
 			}
 		}
 //		System.out.println(change);
-		
+	}
+	
+	public static double convertToDouble(String word) {
+		double num = Double.parseDouble(word);
+		return num;
 	}
 	
 }
-
-//		In the cash register we will calculate the amount of change
-//		returned to a customer based on the purchase price and the amount 
-//		tendered. We will also notify the attendant how many of each piece 
-//		of currency ($20 ,$10 ,$5 ,$1, .25c, .10c, .05c, .01c) is needed to 
-//		make the change for the customer. Change should be provided using the 
-//		largest bill and coin denominations as possible. Denominations that 
-//		are not used should not be displayed.
-//		Hint: Mod operator
-
-//		User Story #1
-//		The user is prompted asking for the price of the item.
-//		User Story #2
-//		The user is then prompted asking how much money was tendered by the customer.
-//		User Story #3
-//		Display an appropriate message if the customer provided too little money or 
-//		the exact amount.
-//		User Story #4
-//		If the amount tendered is more than the cost of the item, display the number
-//		of bills and coins that should be given to the customer.
-//		You will be given either a pass or fail based on whether your code works given 
-//		all of the following test conditions:
-
-//		Amount: .67, Tendered: .50, Result: Error message
-//		Amount: .67, Tendered: 1.00, Result: 1 quarter, 1 nickel, 3 pennies.
-//		Amount: .59, Tendered: 1.00, Result: 1 quarter, 1 dime, 1 nickel, 1 penny.
-//		Amount: 3.96, Tendered: 20.00, Result: 1 ten dollar bill, 1 five dollar bill, 
-//		1 one dollar bill, 4 pennies.
-//		Amount: any amount less than 20.00, Tendered: anything greater than amount:
-//		correct denominations for correct change.
